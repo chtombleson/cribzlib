ca13cd1a1ede1073a2e762f5ffb7eb6dda808666
Christopher Tombleson <chtombleson@gmail.com>
2011-12-07 09:31:28 +1300

    Fixing loader class.

--- cribzlib.php
+++ cribzlib.php
@@ -1,20 +1,20 @@
 <?php
 class CribzLib {
     private $modules = array(
-        'Ajax' => 'ajax/ajax.php'),
-        'Cookie' => 'cookie/cookies.php'),
-        'CountryCodes' => 'misc/countries.php',
-        'Database' => 'database/database.php',
-        'Email' => 'email/email.php',
+        'Ajax' => array('ajax/ajax.php'),
+        'Cookie' => array('cookie/cookies.php'),
+        'CountryCodes' => array('misc/countries.php'),
+        'Database' => array('database/database.php'),
+        'Email' => array('email/email.php'),
         'Form' => array('forms/formfields.php','forms/form.php'),
-        'HtmlFilter' => 'htmlfilter/htmlfilter.php',
-        'I18N' => 'i18n/i18n.php',
-        'LanguageCodes' => 'misc/langs.php',
-        'Page' => 'page/page.php',
-        'Session' => 'session/sessions.php',
+        'HtmlFilter' => array('htmlfilter/htmlfilter.php'),
+        'I18N' => array('i18n/i18n.php'),
+        'LanguageCodes' => array('misc/langs.php'),
+        'Page' => array('page/page.php'),
+        'Session' => array('session/sessions.php'),
         'Spellchecker' => array('spellchecker/spellchecker.php', 'misc/langs.php', 'misc/countries.php'),
         'Template' => array('template/template_compiler.php','template/template.php'),
-        'Tidy' => 'tidy/tidy.php'
+        'Tidy' => array('tidy/tidy.php')
     );
 
     function loadModule($name) {
