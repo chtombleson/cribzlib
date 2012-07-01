97bcca6f2f1694a3c399f73d012e9e82c357bb84
Christopher Tombleson <chtombleson@gmail.com>
2012-07-01 20:41:31 +1200

    Added Documentation to the Cron Class

--- lib/cron/cron.php
+++ lib/cron/cron.php
@@ -1,17 +1,78 @@
 <?php
+/*
+*   This file is part of CribzLib.
+*
+*    CribzLib is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU General Public License as published by
+*    the Free Software Foundation, either version 3 of the License, or
+*    (at your option) any later version.
+*
+*    CribzLib is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU General Public License for more details.
+*
+*    You should have received a copy of the GNU General Public License
+*    along with CribzLib.  If not, see <http://www.gnu.org/licenses/>.
+*/
+/**
+* @package      CribzLib
+* @subpackage   Cribz Cron
+* @author       Christopher Tombleson
+* @copyright    Copyright 2012 onwards
+*/
 require_once(dirname(dirname(__FILE__)).'/mvc/model.php');
 
 class CribzCron {
+    /**
+    * Database
+    *
+    * @var CribzDatabase
+    */
     private $database;
+
+    /**
+    * Table
+    *
+    * @var string
+    */
     private $table;
+
+    /**
+    * Jobs
+    *
+    * @var array
+    */
     private $jobs = array();
+
+    /**
+    * Db Connected
+    *
+    * @var bool
+    */
     private $dbconnected = false;
 
+    /**
+    * Construct
+    * Create a new instance of Cribz Cron
+    *
+    * @param CribzDatabase  $database   Cribz Database Object.
+    * @param string         $table      Name of table in the database.
+    */
     function __construct($database, $table) {
         $this->database = $database;
         $this->table = $table;
     }
 
+    /**
+    * Add Job
+    * Add a job to the cron queue
+    *
+    * @param string  $name      Name for cron job.
+    * @param array   $function  Array with function name and any data to be passed to function.
+    *
+    * @return bool true
+    */
     function addJob($name, $function) {
         if (isset($this->jobs[$name])) {
             throw new CribzCronException("Job with the name {$name} already exists.", 0);
@@ -26,6 +87,15 @@ class CribzCron {
         return true;
     }
 
+    /**
+    * Update Job
+    * Update a Job
+    *
+    * @param string $name       Name of cron job to update.
+    * @param array  $function   Array with function name and any data to be passed to function.
+    *
+    * @return bool true
+    */
     function updateJob($name, $function) {
         if (!$this->dbconnected) {
             $this->database->connect();
@@ -54,6 +124,14 @@ class CribzCron {
         return true;
     }
 
+    /**
+    * Remove Job
+    * Remove a job from the cron queue
+    *
+    * @param string $name   Name of cron job to remove.
+    *
+    * @return true on success or false on error
+    */
     function removeJob($name) {
         if (!$this->dbconnected) {
             $this->database->connect();
@@ -73,6 +151,14 @@ class CribzCron {
         return $this->database->delete($this->table, array('id' => $result->id));
     }
 
+    /**
+    * Run Job
+    * Run a job
+    *
+    * @param string $name   Name of cron job to run.
+    *
+    * @return mixed return value from the cron job functions
+    */
     function runJob($name) {
         if (isset($this->jobs[$name])) {
             $data = unserialize($this->jobs[$name]->function);
@@ -104,6 +190,12 @@ class CribzCron {
         }
     }
 
+    /**
+    * Run All
+    * Run all cron jobs
+    *
+    * @return array of return values from functions or null on empty run
+    */
     function runAll() {
         if (!$this->dbconnected) {
             $this->database->connect();
@@ -113,10 +205,13 @@ class CribzCron {
         $results = $this->database->select($this->table, array(), 'name');
 
         if (!empty($results)) {
+            $returnval = array();
             foreach ($results as $result) {
-                $this->runJob($result->name);
+                $returnval[$result->name] = $this->runJob($result->name);
             }
+            return $returnval;
         }
+        return null;
     }
 }
 
