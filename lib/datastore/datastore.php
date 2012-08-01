9e27a55704b1c1949408f98d9fdb71d60c9bf153
Christopher Tombleson <chtombleson@gmail.com>
2012-08-02 10:58:28 +1200

    Cribz Datastore: Added Documentation

--- lib/datastore/datastore.php
+++ lib/datastore/datastore.php
@@ -1,30 +1,120 @@
 <?php
+/*
+*   This file is part of CribzLib.
+*
+*    CribzLib is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU General Public License as published by
+*    the Free Software Foundation, either version 3 of the License, or
+*    (at your option) any later version.
+*
+*    CribzLib is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU General Public License for more details.
+*
+*    You should have received a copy of the GNU General Public License
+*    along with CribzLib.  If not, see <http://www.gnu.org/licenses/>.
+*/
+/**
+* @package      CribzLib
+* @subpackage   Cribz Datastore
+* @author       Christopher Tombleson
+* @copyright    Copyright 2012 onwards
+*/
 class CribzDatastore {
+    /**
+    * Store
+    *
+    * @var array
+    */
     private $store = array();
+
+    /**
+    * Name Index
+    *
+    * @var array
+    */
     private $nameindex = array();
+
+    /**
+    * Id Index
+    *
+    * @var array
+    */
     private $idindex = array();
+
+    /**
+    * Hash Index
+    *
+    * @var array
+    */
     private $hashindex = array();
+
+    /**
+    * ID Count
+    *
+    * @var int
+    */
     private $idcount = 0;
+
+    /**
+    * Store Size
+    *
+    * @var int
+    */
     private $storesize = 1;
+
+    /**
+    * Valid Fields
+    *
+    * @var array
+    */
     private $validfields = array('id', 'name', 'hash', 'type', 'data', 'timecreated', 'timemodified');
+
+    /**
+    * Persistent
+    *
+    * @var bool
+    */
     private $persistent;
+
+    /**
+    * Data File Path
+    *
+    * @var string
+    */
     private $datafilepath;
 
-
-    function __construct($size, $persistent=false, $datafilepath='') {
+    /**
+    * Construct
+    * Create a new instance of Cribz Datastore.
+    *
+    * @param int   $size         The size of the data store, how many items can be stored.
+    * @param bool  $persistent   If true the current datastore will be wriiten to file when class is destroyed.
+    * @param strng $datafilepath Path to file to write data to.
+    * @param bool  $load_data    Load data from data file.
+    */
+    function __construct($size, $persistent=false, $datafilepath='', $load_data=false) {
         $this->storesize = $size;
         $this->persistent = $persistent;
         $this->datafilepath = $datafilepath;
 
-        if (file_exists($this->datafilepath)) {
-            $this->load_json($this->datafilepath);
+        if ($load_data) {
+            if (file_exists($this->datafilepath) && is_readable($this->datafilepath)) {
+                $this->load_data($this->datafilepath);
+            } else {
+                throw new CribzDatastoreException("Data file must exist and be readable.", 7);
+            }
         }
     }
 
+    /**
+    * Destruct
+    * Destory the class.
+    */
     function __destruct() {
         if ($this->persistent) {
-            $json = serialize($this->store);
-            file_put_contents($this->datafilepath, $json);
+            $this->save_data($this->datafilepath);
         }
 
         unset($this->store, $this->nameindex, $this->idindex, $this->hashindex);
@@ -32,24 +122,51 @@ class CribzDatastore {
         unset($this->datafilepath);
     }
 
-    function load_json($path) {
-        $json = file_get_contents($path);
-        $data = unserialize($json);
+    /**
+    * Load Data
+    * Load data the has been written to a file.
+    *
+    * @param string $path   Path to file to load.
+    * @param bool   $resize If true resize the data store to allow for data to be loaded.
+    *
+    * @return true on success or throws CribzDatastoreException.
+    */
+    function load_data($path, $resize=false) {
+        $data = file_get_contents($path);
+        $stored_data = unserialize($data);
 
-        if ($this->is_full()) {
-            throw new CribzDatastoreException("Data store is full.", 0);
+        if (!$resize) {
+            if ($this->is_full()) {
+                throw new CribzDatastoreException("Data store is full.", 0);
+            }
+
+            if ((count($stored_data) + count($this->store)) >= $this->storesize) {
+                throw new CribzDatastoreException("Data store is not large enough to hold all the data, resize it and try again.", 6);
+            }
+        } else {
+            if ($this->is_full()) {
+                $newsize = count($stored_data) + $this->storesize;
+                $this->resize($newsize);
+            } else if ((count($stored_data) + count($this->store)) >= $this->storesize) {
+                $newsize = count($stored_data) + count($this->store);
+                $this->resize($newsize);
+            }
         }
 
-        if ((count($data) + (count($this->store) - 1)) >= $this->storesize) {
-            throw new CribzDatastoreException("Data store is not large enough to hold all the data, resize it and try again.", 6);
-        }
-
-        $this->store = array_merge($this->store, $data);
+        $this->store = array_merge($this->store, $stored_data);
 
         foreach ($this->store as $hash => $value) {
-            $this->hashindex[] = $hash;
-            $this->nameindex[$value->name] = $hash;
-            $this->idindex[$value->id] = $hash;
+            if (!$this->id_exists($value->id) && !$this->name_exists($value->name)){
+                $this->hashindex[] = $hash;
+                $this->nameindex[$value->name] = $hash;
+                $this->idindex[$value->id] = $hash;
+            } else if ($this->id_exists($value->id)) {
+                $id = count($this->store) + 1;
+                $this->hashindex[] = $hash;
+                $this->nameindex[$value->name] = $hash;
+                $this->idindex[$id] = $hash;
+                $this->store[$hash]->id = $id;
+            }
         }
 
         $this->idcount = count($this->store);
@@ -58,6 +175,31 @@ class CribzDatastore {
         return true;
     }
 
+    /**
+    * Save Data
+    * Write data out to file.
+    *
+    * @param string $path   Path to file to write to.
+    *
+    * @return true on success or throws CribzDatastoreException.
+    */
+    function save_data($path) {
+        $data = serialize($this->store);
+        if (!file_put_contents($this->datafilepath, $data)) {
+            throw new CribzDatastoreException("Could not write data to data file.", 8);
+        }
+        return true;
+    }
+
+    /**
+    * Add
+    * Adds to the data store.
+    *
+    * @param string $name   Name of item.
+    * @param mixed  $value  Data to store.
+    *
+    * @return int id of stored item or throws CribzDatastoreException.
+    */
     function add($name, $value) {
         if ($this->is_full()) {
             throw new CribzDatastoreException("Data store is full.", 0);
@@ -89,6 +231,15 @@ class CribzDatastore {
         return $id;
     }
 
+    /**
+    * Update By Name
+    * Update an item in the data store by name.
+    *
+    * @param string $name   Name of item to update.
+    * @param mixed  $value  Updated data.
+    *
+    * @return true on success or throws CribzDatastoreException.
+    */
     function update_by_name($name, $value) {
         if (!$this->name_exists($name)) {
             throw new CribzDatastoreException("The name: {$name}, does not exists in the data store.", 2);
@@ -98,6 +249,15 @@ class CribzDatastore {
         return $this->update_by_hash($hash, $value);
     }
 
+    /**
+    * Update By ID
+    * Update an item in the data store by ID.
+    *
+    * @param int    $id     ID of item to update.
+    * @param mixed  $value  Updated data.
+    *
+    * @return true on success or throws CribzDatastoreException.
+    */
     function update_by_id($id, $value) {
         if (!$this->id_exists($id)) {
             throw new CribzDatastoreException("The id: {$id}, does not exists in the data store.", 3);
@@ -107,6 +267,15 @@ class CribzDatastore {
         return $this->update_by_hash($hash, $value);
     }
 
+    /**
+    * Update By Hash
+    * Update an item in the data store by hash.
+    *
+    * @param string $hash   Hash of item to update.
+    * @param mixed  $value  Updated data.
+    *
+    * @return true on success or throws CribzDatastoreException.
+    */
     function update_by_hash($hash, $value) {
         if (!$this->hash_exists($hash)) {
             throw new CribzDatastoreException("The hash: {$hash}, does not exists in the data store.", 4);
@@ -119,6 +288,14 @@ class CribzDatastore {
         return true;
     }
 
+    /**
+    * Delete By Name
+    * Delete an item by name.
+    *
+    * @param string $name   Name of item to delete.
+    *
+    * @return true on success or throws CribzDatastoreException.
+    */
     function delete_by_name($name) {
         if (!$this->name_exists($name)) {
             throw new CribzDatastoreException("The name: {$name}, does not exists in the data store.", 2);
@@ -128,6 +305,14 @@ class CribzDatastore {
         return $this->delete_by_hash($hash);
     }
 
+    /**
+    * Delete By ID
+    * Delete an item by ID.
+    *
+    * @param int $id ID of item to delete.
+    *
+    * @return true on success or throws CribzDatastoreException.
+    */
     function delete_by_id($id) {
         if (!$this->id_exists($id)) {
             throw new CribzDatastoreException("The id: {$id}, does not exists in the data store.", 3);
@@ -137,6 +322,14 @@ class CribzDatastore {
         return $this->delete_by_hash($hash);
     }
 
+    /**
+    * Delete By Hash
+    * Delete an item by hash.
+    *
+    * @param string $hash   Hash of item to delete.
+    *
+    * @return true on success or throws CribzDatastoreException.
+    */
     function delete_by_hash($hash) {
         if (!$this->hash_exists($hash)) {
             throw new CribzDatastoreException("The hash: {$hash}, does not exists in the data store.", 4);
@@ -146,6 +339,13 @@ class CribzDatastore {
         return true;
     }
 
+    /**
+    * Delete By Names
+    * Delete multiple items by name.
+    *
+    * @param array $names   Array of names to delete.
+    * @see Delete By Name
+    */
     function delete_by_names(array $names) {
         foreach ($names as $name) {
             $this->delete_by_name($name);
@@ -153,6 +353,13 @@ class CribzDatastore {
         return true;
     }
 
+    /**
+    * Delete By IDs
+    * Delete multiple items by id.
+    *
+    * @param array $ids   Array of ids to delete.
+    * @see Delete By ID
+    */
     function delete_by_ids(array $ids) {
         foreach ($ids as $id) {
             $this->delete_by_id($id);
@@ -160,6 +367,13 @@ class CribzDatastore {
         return true;
     }
 
+    /**
+    * Delete By Hashes
+    * Delete multiple items by hash.
+    *
+    * @param array $hashes   Array of hashes to delete.
+    * @see Delete By Hash
+    */
     function delete_by_hashes(array $hashes) {
         foreach ($hashes as $hash) {
             $this->delete_by_hash($hash);
@@ -167,6 +381,15 @@ class CribzDatastore {
         return true;
     }
 
+    /**
+    * Get By Name
+    * Get an item by name.
+    *
+    * @param string $name    Name of item to get.
+    * @param string $fields  Fields to get. (Optional)
+    *
+    * @return stdclass with the data on success or null on failure, throws CribzDatastoreException.
+    */
     function get_by_name($name, $fields=null) {
         if (!$this->name_exists($name)) {
             throw new CribzDatastoreException("The name: {$name}, does not exists in the data store.", 2);
@@ -176,6 +399,15 @@ class CribzDatastore {
         return $this->get_by_hash($hash, $fields);
     }
 
+    /**
+    * Get By ID
+    * Get an item by ID.
+    *
+    * @param string $id      ID of item to get.
+    * @param string $fields  Fields to get. (Optional)
+    *
+    * @return stdclass with the data on success or null on failure, throws CribzDatastoreException.
+    */
     function get_by_id($id, $fields=null) {
         if (!$this->id_exists($id)) {
             throw new CribzDatastoreException("The id: {$id}, does not exists in the data store.", 3);
@@ -185,6 +417,15 @@ class CribzDatastore {
         return $this->get_by_hash($hash, $fields);
     }
 
+    /**
+    * Get By Hash
+    * Get an item by hash.
+    *
+    * @param string $hash    Hash of item to get.
+    * @param string $fields  Fields to get. (Optional)
+    *
+    * @return stdclass with the data on success or null on failure, throws CribzDatastoreException.
+    */
     function get_by_hash($hash, $fields=null) {
         if (!$this->hash_exists($hash)) {
             throw new CribzDatastoreException("The hash: {$hash}, does not exists in the data store.", 4);
@@ -206,6 +447,14 @@ class CribzDatastore {
         return $this->store[$hash];
     }
 
+    /**
+    * Get By Names
+    * Get multiple items by name.
+    *
+    * @param array  $names   Array of names.
+    * @param string $fields  Fields to get. (Optional)
+    * @see Get By Name
+    */
     function get_by_names(array $names, $fields=null) {
         $returnarr = array();
         foreach ($names as $name) {
@@ -217,6 +466,14 @@ class CribzDatastore {
         return empty($returnarr) ? null : $returnarr;
     }
 
+    /**
+    * Get By IDs
+    * Get multiple items by ID.
+    *
+    * @param array  $ids     Array of ids.
+    * @param string $fields  Fields to get. (Optional)
+    * @see Get By ID
+    */
     function get_by_ids(array $ids, $fields=null) {
         $returnarr = array();
         foreach ($ids as $id) {
@@ -228,6 +485,14 @@ class CribzDatastore {
         return empty($returnarr) ? null : $returnarr;
     }
 
+    /**
+    * Get By Hashes
+    * Get multiple items by hash.
+    *
+    * @param array  $hashes  Array of hashes.
+    * @param string $fields  Fields to get. (Optional)
+    * @see Get By Hash
+    */
     function get_by_hashes(array $hashes, $fields=null) {
         $returnarr = array();
         foreach ($hashes as $hash) {
@@ -239,6 +504,12 @@ class CribzDatastore {
         return empty($returnarr) ? null : $returnarr;
     }
 
+    /**
+    * Is Full
+    * Checks to see if the data store is full.
+    *
+    * @return true if the data store is full or false if there is still room.
+    */
     function is_full() {
         if (count($this->store) < $this->storesize) {
             return false;
@@ -247,6 +518,14 @@ class CribzDatastore {
         return true;
     }
 
+    /**
+    * Hash Exists
+    * Checks to see if the hash exists.
+    *
+    * @param string $hash   Hash to check.
+    *
+    * @return true if hash exists or false if it does not exist.
+    */
     function hash_exists($hash) {
         if (in_array($hash, $this->hashindex)) {
             return true;
@@ -255,6 +534,14 @@ class CribzDatastore {
         return false;
     }
 
+    /**
+    * Name Exists
+    * Checks to see if the name exists.
+    *
+    * @param string $name   Name to check.
+    *
+    * @return true if name exists or false if it does not exist.
+    */
     function name_exists($name) {
         if (in_array($name, array_keys($this->nameindex))) {
             return true;
@@ -263,6 +550,14 @@ class CribzDatastore {
         return false;
     }
 
+    /**
+    * ID Exists
+    * Checks to see if the id exists.
+    *
+    * @param int $id    ID to check.
+    *
+    * @return true if id exists or false if it does not exist.
+    */
     function id_exists($id) {
         if (in_array($id, array_keys($this->idindex))) {
             return true;
@@ -271,6 +566,14 @@ class CribzDatastore {
         return false;
     }
 
+    /**
+    * Hash To Name
+    * Get the name of the item with specified hash.
+    *
+    * @param string $hash   Hash of item you wat to get the name of.
+    *
+    * @return string name of item on success or null if name not found.
+    */
     function hash_to_name($hash) {
         $name = array_search($hash, $this->nameindex);
 
@@ -281,6 +584,14 @@ class CribzDatastore {
         return $name;
     }
 
+    /**
+    * Hash To ID
+    * Get the id of the item with specified hash.
+    *
+    * @param string $hash   Hash of item you wat to get the id of.
+    *
+    * @return int id of item on success or null if id not found.
+    */
     function hash_to_id($hash) {
         $id = array_search($hash, $this->idindex);
 
@@ -291,6 +602,14 @@ class CribzDatastore {
         return $id;
     }
 
+    /**
+    * ID To Hash
+    * Get the hash of the item with specified ID.
+    *
+    * @param int $id    ID of item you want to get hash of.
+    *
+    * @return string hash of item on success or null if ID not found.
+    */
     function id_to_hash($id) {
         if ($this->id_exists($id)) {
             return $this->idindex[$id];
@@ -299,6 +618,14 @@ class CribzDatastore {
         return null;
     }
 
+    /**
+    * Name To Hash
+    * Get the hash of the item with specified name.
+    *
+    * @param string $name   Name of item you want to get hash of.
+    *
+    * @return string hash of item on success or null if name not found.
+    */
     function name_to_hash($name) {
         if ($this->name_exists($name)) {
             return $this->nameindex[$name];
@@ -307,6 +634,14 @@ class CribzDatastore {
         return null;
     }
 
+    /**
+    * ID To name
+    * Get the name of the item with specified ID.
+    *
+    * @param int $id    ID of item you want to get the name of.
+    *
+    * @return string name of item on success or null if id does not exists.
+    */
     function id_to_name($id) {
         $hash = $this->id_to_hash($id);
 
@@ -318,6 +653,14 @@ class CribzDatastore {
         return null;
     }
 
+    /**
+    * Name To ID
+    * Get ID of item with specified name.
+    *
+    * @param string $name   Name of item you want to get ID of.
+    *
+    * @return int id of item onn success or null if name not found.
+    */
     function name_to_id($name) {
         $hash = $this->name_to_hash($name);
 
@@ -329,6 +672,14 @@ class CribzDatastore {
         return null;
     }
 
+    /**
+    * Resize
+    * Resize the data store.
+    *
+    * @param int $newsize   New size of data store.
+    *
+    * @return throws CribzDatastoreException on error.
+    */
     function resize($newsize) {
         if ($newsize <= count($this->store)) {
             throw new CribzDatastoreException("Cannot shrink the size of the data store.", 5);
